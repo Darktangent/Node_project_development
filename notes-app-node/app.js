f4376c42cc9fcc73fe9debb6bfa51642a9b3c3fc
Rohan <c1ph3r9@gmail.com>
2018-09-21 22:47:55 -0500

    2nd batch of files that provides functionality to addNotes fn

--- notes-app-node/app.js
+++ notes-app-node/app.js
@@ -1,13 +1,30 @@
-console.log("Starting app.js")
-const fs = require('fs')
-const os=require('os')
-const notes=require('./notes')
+console.log("Starting app.js");
+const fs = require("fs");
+// const os=require('os')
+const notes = require("./notes");
+const _ = require("lodash");
+const yargs=require('yargs')
 
-var user= os.userInfo();
-console.log(user);
-
-fs.appendFile('./greetings.txt', `hello ${user.username} you are ${notes.age}` ,(err)=>{
-    if(err){
-        console.log("Unable to write file")
-    }
-})
+const argv=yargs.argv
+var command = argv._[0];
+// console.log("Command: ", command);
+console.log("Yargs", argv)
+if (command === "add") {
+  var note= notes.addNote(argv.title, argv.body)
+  if (note){
+    console.log(note.title, note.body)
+    console.log("------")
+    console.log(`Title: ${note.title}`)
+    console.log(`Body: ${note.body}`)
+  } else{
+    console.log("Note title already exists")
+  }
+} else if (command === "list") {
+  notes.getAll()
+} else if (command === "read") {
+  notes.getNote(argv.title);
+} else if (command === "remove") {
+  notes.removeNote(argv.title)
+} else {
+  console.log("Command not found");
+}
--- notes-app-node/notes.js
+++ notes-app-node/notes.js
@@ -1,3 +1,55 @@
-console.log("Starting notes.js")
+console.log("Starting notes.js");
+const fs= require('fs')
 
-module.exports.age=25
+
+var fetchNotes=()=>{
+  try{
+    var notesString=fs.readFileSync('notes-data.json')
+    return JSON.parse(notesString)
+  }catch(err){
+    return []
+  }
+  
+}
+var saveNotes=(notes)=>{
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+}
+
+const addNote = (title,body) => {
+  var notes=fetchNotes()
+  var note= {
+    title,body
+  }
+  var duplicateNotes=notes.filter((note)=>{
+    return note.title===title
+  })
+  if(duplicateNotes.length===0){
+    notes.push(note)
+    saveNotes(notes)
+    return note;
+  
+  }
+
+
+};
+const getAll=()=>{
+  console.log("Getting all notes")
+}
+const getNote=(title)=>{
+  console.log ("Getting note", title)
+}
+const removeNote=(title)=>{
+  //fetch note
+  //filternotes removing the one with title of arg
+  //save new notes
+  var notes=fetchNotes()
+  notes.filter((note)=>{
+    return note.title!==title
+  })
+  saveNotes(notes)
+
+
+}
+module.exports = {
+  addNote, getAll, getNote,removeNote
+};
